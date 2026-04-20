cbe443f418fe187390f094063d4c4a5bc043052f
AzizAlizade473 <elizadeeziz2006@gmail.com>
2026-04-20 16:56:38 +0000

    add 2-arguments.js

--- javascript-warm_up/2-arguments.js
+++ javascript-warm_up/2-arguments.js
@@ -1,16 +1,8 @@
 #!/usr/bin/node
-
-// prints a message depending of the number of arguments passed:
-// if no argument passed prints no argument
-// if arguments passed prints argument found.
-
-const string = process.argv.length;
-if (string === 2) {
+if (process.argv.length === 2) {
   console.log('No argument');
-} else if (string === 3) {
+} else if (process.argv.length === 3) {
   console.log('Argument found');
 } else {
   console.log('Arguments found');
 }
-
-// ref : https://riptutorial.com/node-js/example/10945/process-argv-command-line-arguments
