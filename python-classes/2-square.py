ef36380d0d565e06a522f778fe2eac98d4500356
Aziz Alizade <elizadeeziz2006@gmail.com>
2026-04-20 14:47:29 +0400

    Update 2-square.py

--- python-classes/2-square.py
+++ python-classes/2-square.py
@@ -1,23 +1,21 @@
 #!/usr/bin/python3
-# -*- coding: UTF-8 -*-
-"""
-1-square.py: is a class Square that defines a square
-"""
-
 
 class Square:
-    """class Square that defines a square
-
-        Attributes:
-        attr1 (size): Size of the Square.
-
-    """
+    """Defines a square by its size with validation"""
 
     def __init__(self, size=0):
-        """Initializer with default size = 0"""
-        if type(size) is not int:
-            raise TypeError("size must be an integer")
-        if size < 0:
-            raise ValueError("size must be >= 0")
-        self.__size = size
+        """Initialize a new Square
 
+        Args:
+            size (int): The size of the square. Default is 0.
+
+        Raises:
+            TypeError: If size is not an integer
+            ValueError: If size is less than 0
+        """
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = size
